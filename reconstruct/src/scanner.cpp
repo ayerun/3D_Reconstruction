eac96cd4285bc7c5cfd5056db97d6b143828755e
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-02-09 15:29:52 -0600

    transforming and concatenating point cloud - code compiles but seg faults going to try to change pointcloud pointers to messages

--- reconstruct/src/scanner.cpp
+++ reconstruct/src/scanner.cpp
@@ -16,22 +16,26 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/registration/icp.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
+#include <iostream>
 
 //global variables
 static ros::Subscriber pc_sub;
 static ros::Publisher pc_pub;
 
+static sensor_msgs::PointCloud2Ptr PCold;
+static sensor_msgs::PointCloud2Ptr PCtrans;
 static sensor_msgs::PointCloud2Ptr PCfused;
 
 static pcl::PCLPointCloud2 cloud_in_pcl;    //source
 static pcl::PCLPointCloud2 cloud_out_pcl;   //target
-static pcl::PCLPointCloud2 cloud_fused_pcl; //aligned
 static pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);       //source
 static pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);      //target
-static pcl::PointCloud<pcl::PointXYZ> cloud_fused;                                              //aligned
+static pcl::PointCloud<pcl::PointXYZ> cloud_fused;                                              //fused
 static pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+static Eigen::Matrix4f transformation;
 
-static const int frequency = 2;
+static const int frequency = 5;
 static bool first = true;
 
 /*
@@ -40,12 +44,14 @@ transform PointCloud
 Concatenate PointCloud
 publish concatenated pointcloud
 */
-void pcCallback(const sensor_msgs::PointCloud2Ptr &PCmsg)
+void pcCallback(const sensor_msgs::PointCloud2Ptr &PCnew)
 {
     if(first)
     {
         //store pointcloud
-        PCfused = PCmsg;
+        PCold = PCnew;
+        PCfused = PCnew;
+        PCtrans = PCnew;
 
         //publish pointcloud
         pc_pub.publish(PCfused);
@@ -55,8 +61,8 @@ void pcCallback(const sensor_msgs::PointCloud2Ptr &PCmsg)
     else
     {
         //convert to PCLPointCloud2 then to PointCloud<pcl::PointXYZ>
-        pcl_conversions::toPCL(*PCmsg,cloud_in_pcl);
-        pcl_conversions::toPCL(*PCfused,cloud_out_pcl);
+        pcl_conversions::toPCL(*PCnew,cloud_in_pcl);
+        pcl_conversions::toPCL(*PCold,cloud_out_pcl);
         pcl::fromPCLPointCloud2(cloud_in_pcl,*cloud_in);
         pcl::fromPCLPointCloud2(cloud_out_pcl,*cloud_out);
 
@@ -64,14 +70,24 @@ void pcCallback(const sensor_msgs::PointCloud2Ptr &PCmsg)
         icp.setInputSource(cloud_in);
         icp.setInputTarget(cloud_out);
         icp.align(cloud_fused);
+        transformation = icp.getFinalTransformation();
+        // std::cout << transformation << std::endl;
 
-        //convert back
-        pcl::toPCLPointCloud2(cloud_fused, cloud_fused_pcl);
-        pcl_conversions::fromPCL(cloud_fused_pcl, *PCfused);
+        //Transform Point Cloud
+        pcl_ros::transformPointCloud(transformation, *PCnew, *PCtrans);
+
+        // ROS_ERROR_STREAM(PCtrans->header);
+        // ROS_ERROR_STREAM(PCfused->header);
+
+        //Fuse point clouds
+        pcl::concatenatePointCloud(*PCfused,*PCtrans,*PCfused);
+
 
         //publish pointcloud
-        ROS_ERROR_STREAM("Publishing");
+        ROS_WARN_STREAM("Publishing");
         pc_pub.publish(PCfused);
+
+        PCold = PCnew;
     }
 }
 
@@ -82,7 +98,7 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
 
     //initialize subscriber
-    pc_sub = nh.subscribe("/camera/depth/color/points", 10, pcCallback);
+    pc_sub = nh.subscribe("/voxel_grid/output", 10, pcCallback);
     pc_pub = nh.advertise<sensor_msgs::PointCloud2>("/fused", 10, true);
 
     //looping frequency
@@ -90,6 +106,8 @@ int main(int argc, char** argv)
 
     //icp settings
     icp.setMaximumIterations(50);
+
+    transformation = Eigen::Matrix4f::Identity();
 
     while(ros::ok())
     {
