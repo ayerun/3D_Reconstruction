8c77444e6045e0a71e6c908d431aea4b9bb5a838
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-02-11 15:14:31 -0600

    started node for triagulating mesh node subscribes to pointcloud map

---
+++ reconstruct/src/triangulate.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+static ros::Subscriber pc_sub;
+static sensor_msgs::PointCloud2Ptr scan;
+
+void pcCallback(const sensor_msgs::PointCloud2Ptr &PCmsg)
+{
+    scan = PCmsg;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc,argv,"triangulate");
+    ros::NodeHandle nh;
+
+    pc_sub = nh.subscribe("/rtabmap/cloud_map", 10, pcCallback);
+
+    while(ros::ok())
+    {
+        ros::spinOnce();
+    }
+
+    return 1;
+}
+
+//bill strong works with 3D scanning
